a4bfb03538556aa97c6c4fe502cc4b6072c14659
angelogiovonni22-collab <angelogiovonni22@gmail.com>
2026-08-09 19:31:37 -0400

    Route Persistent Orion through Realtime runtime

--- components/orion/persistent/index.ts
+++ components/orion/persistent/index.ts
@@ -1,3 +1,3 @@
-export { PersistentOrion } from "./PersistentOrion";
+export { PersistentOrionRuntime as PersistentOrion, useOrionRealtimeRuntime } from "./OrionRealtimeRuntime";
 export { getPersistentOrionFixture, getPersistentOrionPalette } from "./fixtures";
 export type { PersistentOrionFixture, PersistentOrionPalette, PersistentOrionStateId } from "./types";
