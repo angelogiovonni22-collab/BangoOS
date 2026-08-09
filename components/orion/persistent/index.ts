d343b1e6bda8da18aeb082c147932670b02cd0bf
angelogiovonni22-collab <angelogiovonni22@gmail.com>
2026-08-09 19:32:39 -0400

    Export Persistent Orion Realtime wrapper

--- components/orion/persistent/index.ts
+++ components/orion/persistent/index.ts
@@ -1,3 +1,4 @@
-export { PersistentOrionRuntime as PersistentOrion, useOrionRealtimeRuntime } from "./OrionRealtimeRuntime";
+export { PersistentOrionRuntime as PersistentOrion } from "./PersistentOrionRuntime";
+export { useOrionRealtimeRuntime } from "./OrionRealtimeRuntime";
 export { getPersistentOrionFixture, getPersistentOrionPalette } from "./fixtures";
 export type { PersistentOrionFixture, PersistentOrionPalette, PersistentOrionStateId } from "./types";
